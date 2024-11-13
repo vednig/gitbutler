8a14a72de56e0f2bd5d16df4e48e10cf81610a24
ndom91 <yo@ndo.dev>
2024-11-13 19:03:55 +0100

    feat: storybook 8.4 upgrade and Svelte CSF

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,21 +1,25 @@
 import { dirname, join } from 'path';
-import type { StorybookConfig } from '@storybook/sveltekit';
+import type { StorybookConfig } from '@storybook/svelte-vite';
 
 const config: StorybookConfig = {
 	stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx|svelte)'],
 	addons: [
+		{
+			name: '@storybook/addon-svelte-csf',
+			options: {
+				legacyTemplate: true
+			}
+		},
 		getAbsolutePath('@storybook/addon-links'),
 		getAbsolutePath('@storybook/addon-essentials'),
 		getAbsolutePath('storybook-dark-mode'),
 		getAbsolutePath('@storybook/experimental-addon-test')
 	],
-	framework: {
-		name: getAbsolutePath('@storybook/sveltekit'),
-		options: {}
-	}
+	framework: '@storybook/sveltekit'
 };
-export default config;
 
 function getAbsolutePath(value: string): any {
 	return dirname(require.resolve(join(value, 'package.json')));
 }
+
+export default config;
---
+++ packages/ui/src/stories/button/DemoButton.stories.svelte
@@ -0,0 +1,32 @@
+<script module lang="ts">
+	import Button from '$lib/Button.svelte';
+	import iconsJson from '$lib/data/icons.json';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Inputs / Button',
+		component: Button,
+		args: {
+			type: 'button',
+			size: 'tag',
+			style: 'pop'
+		},
+		argTypes: {
+			icon: { control: 'select', options: Object.keys(iconsJson) },
+			type: { control: 'select', options: ['button', 'submit', 'reset'] },
+			width: { control: 'text' },
+			size: { control: 'select', options: ['tag', 'button', 'cta'] },
+			align: {
+				control: 'select',
+				options: ['center', 'left', 'right', 'space-between']
+			},
+			style: {
+				control: 'select',
+				options: ['neutral', 'ghost', 'pop', 'success', 'error', 'warning', 'purple']
+			},
+			kind: { control: 'select', options: ['solid', 'soft'] }
+		}
+	});
+</script>
+
+<Story name="Demo Button" />
